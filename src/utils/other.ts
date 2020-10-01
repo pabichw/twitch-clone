3893bebf4353216b113947eba3a533e7097fc21d
pabichw <pabichwiktor@gmail.com>
2020-10-01 22:59:38 +0200

    avatars resolution fix

--- src/utils/other.ts
+++ src/utils/other.ts
@@ -1,4 +1,5 @@
 export const getImageOfSize = (src: string, width: number, height: number) =>
   src
     .replace('{height}', height.toString())
-    .replace('{width}', width.toString());
+    .replace('{width}', width.toString())
+    .replace(/\d{1,3}x\d{1,3}/, `${width}x${height}`); // just in case
